b30b91b3719ecf6decd46debc79197f0eea8d355
이재원 <hongin@ijaewon-ui-MacBookPro.local>
2021-01-23 02:13:32 +0900

    MODIFY app_dev.js

--- app_dev.js
+++ app_dev.js
@@ -1,37 +1,35 @@
 require("dotenv").config();
-const https = require('https');
-const fs = require('fs');
+const https = require("https");
+const fs = require("fs");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 //cert.pem랑 key.pem은 배포할 때 경로 맞춰줍시다.
-const privateKey = fs.readFileSync(__dirname + "/../../key.pem", "utf8");
-const certificate = fs.readFileSync(__dirname + "/../../cert.pem", "utf8");
+const privateKey = fs.readFileSync(__dirname + "/../key.pem", "utf8");
+const certificate = fs.readFileSync(__dirname + "/../cert.pem", "utf8");
 const credentials = { key: privateKey, cert: certificate };
 // const uuid = require('uuid/v4');
 
-const router = require('./routes/route');
-const express = require('express');
+const router = require("./routes/route");
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
-    cors({
-        origin: ['http://localhost:3000'],
-        credentials: true,
-        methods: ["GET", "POST", "OPTIONS"],
-    })
-)
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
 app.use(cookieParser());
-app.use('/', router)
+app.use("/", router);
 
 const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
 const httpsServer = https.createServer(credentials, app);
 
 httpsServer.listen(HTTPS_PORT, () => {
-    console.log("server runnning ", HTTPS_PORT)
+  console.log("server runnning ", HTTPS_PORT);
 });
-
-
 
 module.exports = httpsServer;
